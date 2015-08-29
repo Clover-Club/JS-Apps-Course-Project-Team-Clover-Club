541e68ec59cd5a9715e1638703f652f621cecf33
ivanvasilev <ivanvasilev_90@yahoo.com>
2015-08-29 22:22:42 +0300

    Added the html and logic when logging in, registering and logging out.

--- Video Manager/Video Manager/scripts/authentication.js
+++ Video Manager/Video Manager/scripts/authentication.js
@@ -1,7 +1,18 @@
-var authentication = (function() {
+var authentication = (function () {
 
-    function authenticationSuccess(data) {
-        console.log(data);
+    function authenticationSuccessForLogin(data) {
+        $('.login-div').hide();
+        $('.buttons').hide();
+        $('.main').hide();
+        $('.contacts').hide();
+        $('.empty').css('height', '50px');
+        $('.register').hide();
+        $('.logged-user').show();
+        $('#on-log').show();
+        $('#on-start').hide();
+        $('#user-name').text(data.username);
+        $('#greeting-span').text(data.username);
+        //console.log(data);
         // TODO: relocate to user videos page
     }
 
@@ -10,11 +21,27 @@ var authentication = (function() {
         // TODO: error message
     }
 
+    function authenticationSuccessForRegistering(data) {
+        $('.login-div').hide();
+        $('.buttons').hide();
+        $('.main').hide();
+        $('.contacts').hide();
+        $('.empty').css('height', '50px');
+        $('.register').hide();
+        $('.logged-user').show();
+        $('#on-log').show();
+        $('#on-start').hide();
+        $('#user-name').text($('#username').val());
+        $('#greeting-span').text($('#username').val());
+        //console.log(data);
+        // TODO: relocate to user videos page
+    }
+
     function login() {
         var username = $('#usr').val(),
             password = $('#pwd').val();
 
-        ajaxRequester.login(username, password, authenticationSuccess, authenticationFailed);
+        ajaxRequester.login(username, password, authenticationSuccessForLogin, authenticationFailed);
     }
 
     function register() {
@@ -22,7 +49,7 @@ var authentication = (function() {
             var username = $('#username').val(),
                 password = $('#pass').val();
 
-            ajaxRequester.register(username, password, authenticationSuccess, authenticationFailed);
+            ajaxRequester.register(username, password, authenticationSuccessForRegistering, authenticationFailed);
         }
     }
 
--- Video Manager/Video Manager/scripts/start.js
+++ Video Manager/Video Manager/scripts/start.js
@@ -1,39 +1,54 @@
 ﻿var start = (function start() {
-    $('#login').click(function() {
+    $('#login').click(function () {
         $('.login-div').show();
         $('.buttons').show();
         $('.main').hide();
         $('.contacts').hide();
         $('.empty').css('height', '150px'),
-            $('.register').hide();
+        $('.register').hide();
+        $('.logged-user').hide();
     });
 
-    $('#contacts').click(function() {
+    $('#contacts').click(function () {
         $('.login-div').hide();
         $('.buttons').hide();
         $('.main').hide();
         $('.contacts').show();
         $('.empty').css('height', '100px'),
-            $('.register').hide();
+        $('.register').hide();
+        $('.logged-user').hide();
     });
 
-    $('#home').click(function() {
+    $('#home').click(function () {
         $('.login-div').hide();
         $('.buttons').hide();
         $('.main').show();
         $('.contacts').hide();
         $('.empty').css('height', '200px');
         $('.register').hide();
+        $('.logged-user').hide();
     });
 
-    $('#register-btn').click(function() {
+    $('#register-btn').click(function () {
         $('.login-div').hide();
         $('.buttons').hide();
         $('.main').hide();
         $('.contacts').hide();
         $('.empty').css('height', '150px');
         $('.register').show();
+        $('.logged-user').hide();
+    });
 
+    $('#logout').click(function () {
+        $('.login-div').hide();
+        $('.buttons').hide();
+        $('.main').show();
+        $('.contacts').hide();
+        $('.empty').css('height', '200px');
+        $('.register').hide();
+        $('.logged-user').hide();
+        $('#on-log').hide();
+        $('#on-start').show();
     });
 
     $('#login-btn').on('click', authentication.login);
