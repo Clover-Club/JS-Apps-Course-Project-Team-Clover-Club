379fa62e0909f8360ecb6bd228ba185d08675615
ivanvasilev <ivanvasilev_90@yahoo.com>
2015-08-29 15:31:28 +0300

    Added main.html, some styles and a script.

---
+++ Video Manager/Video Manager/scripts.js
@@ -0,0 +1,15 @@
+﻿$('#login').click(function () {
+    $('.login-div').show();
+    $('.buttons').show();
+    $('.main').hide();
+    $('.contacts').hide();
+    $('.empty').css('height', '150px')
+});
+
+$('#contacts').click(function () {
+    $('.login-div').hide();
+    $('.buttons').hide();
+    $('.main').hide();
+    $('.contacts').show();
+    $('.empty').css('height', '100px')
+});
