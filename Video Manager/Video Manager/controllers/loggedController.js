9c5d50906cbdd87342da7771b113a1cc728ee0ef
ivanvasilev <ivanvasilev_90@yahoo.com>
2015-09-02 23:18:39 +0300

    Added when logging in to show all added videos of the user. Added functionality of the add video button with the categories.

--- Video Manager/Video Manager/controllers/loggedController.js
+++ Video Manager/Video Manager/controllers/loggedController.js
@@ -1,8 +1,26 @@
-var loggedController = (function() {
-	validator.changeNavbar(sessionStorage.currentUser);
+var loggedController = (function () {
+    validator.changeNavbar(sessionStorage.currentUser);
 
-	$('#logout').show();
-	$('#contacts').hide();
+    $('#logout').show();
+    $('#contacts').hide();
 
-	$('#logout').on('click', authentication.logout);
+    $('#logout').on('click', authentication.logout);
+
+    ajaxRequester.getVideos(sessionStorage.userId, videoRenderer.renderAllVideos);
+
+    $('#add-form-btn').click(function () {
+        var url = $("#add-video").val();
+        // create video here
+
+        var videoId = videoUrlParser.parse(url);
+        var userId = sessionStorage.userId;
+        var category = $('#category').val();
+
+        ajaxRequester.createVideo(userId, videoId, videoStorage.setVideoId);
+
+        var videoIdInDatabase = sessionStorage.currentAddedVideo;
+
+        // remove this later
+        ajaxRequester.getVideos(userId, videoRenderer.renderAllVideos);
+    });
 })();
