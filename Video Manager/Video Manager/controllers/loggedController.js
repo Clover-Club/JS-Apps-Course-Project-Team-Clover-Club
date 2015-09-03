c21157a3e0fe4ab7d5307ff8104394b1f4e8791f
Ivaylo Andonov <ivailo.kolev.andonov@gmail.com>
2015-09-03 16:43:47 +0300

    Add style categories and fix locate when is logged

--- Video Manager/Video Manager/controllers/loggedController.js
+++ Video Manager/Video Manager/controllers/loggedController.js
@@ -18,13 +18,25 @@ var loggedController = (function () {
             var list = $('<div />').text(key).attr('id', key.toLowerCase());
             _.each(value, function (video) {
                 videoRenderer.renderSingleVideo($('#users-videos'), video);
-            })
+            });
 
-            $('#users-videos').prepend($('<h5 />').text(key));
+            $('#users-videos')
+                .prepend($('<h4 />')
+                    .css("background-color","#576067")
+                    .css("border-radius","10px")
+                    .css("font-size","1.5em")
+                    .css("border","1px groove grey")
+                    .css("box-shadow","7px 7px 4px #1d2428")
+                    .css("font-family","'Ubuntu', sans-serif;")
+                    .text(key))
+                    .css("color","#faebb8")
+
         })
     }
 
     $('#add-form-btn').click(function () {
+        location.href = '#/logged';
+
         var url = $("#add-video").val();
         // create video here
 
@@ -38,5 +50,9 @@ var loggedController = (function () {
 
         // remove this later
         ajaxRequester.getVideos(userId, showAllVideosInCategories);
+
     });
+
+    location.href = '#/logged';
+
 })();
